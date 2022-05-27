13c6df7323193eb440ba67db4eb8ec6960f90b4a
nguyenduong21 <thien123111999@gmail.com>
2022-05-27 15:03:03 +0700

    Add response for start button

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -126,6 +126,8 @@ function handlePostback(sender_psid, received_postback) {
     response = { "text": "Thanks!" }
   } else if (payload === 'no') {
     response = { "text": "Oops, try sending another image." }
+  } else if (payload === "GET_STARTED") {
+    response = { "text": "OK. Xin chào mừng bạn đến với nhà hàng" };
   }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
