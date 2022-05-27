1266f89bab3983827ea9c4e710a16ded46c0ec37
nguyenduong21 <thien123111999@gmail.com>
2022-05-27 09:27:19 +0700

    deploy heroku

---
+++ src/routes/web.js
@@ -0,0 +1,12 @@
+import express from "express";
+let router = express.Router();
+import HomeController from "../controllers/HomeController";
+let initWebRoutes = (app) => {
+    router.get('/', HomeController.getHomePage);
+
+    app.post('/webhook', HomeController.postWebhook);
+    app.get('/webhook', HomeController.getWebhook);
+    return app.use('/', router);
+}
+
+module.exports = initWebRoutes;
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import viewEngine from "./config/viewEngine";
+import webRoutes from "./routes/web";
+import bodyParser from "body-parser";
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//config view Engine
+viewEngine(app);
+
+//config route for app
+webRoutes(app);
+
+app.listen(PORT, () => {
+    console.log("App running at the port: " + PORT);
+});
