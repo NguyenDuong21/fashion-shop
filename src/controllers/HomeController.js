3ca55f4ba71212577570d3dd023fa1b3e3d9b45a
nguyenduong21 <thien123111999@gmail.com>
2022-05-28 21:02:20 +0700

    Log webhook event

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -19,6 +19,7 @@ let postWebhook = (req, res) => {
         // Gets the body of the webhook event
         let webhook_event = entry.messaging[0];
         
+        console.log(webhook_event);
         // check error
 
         // Get the sender PSID
